c3b74804d62e2bd5e63dc8a484a700f2b4857088
liyang453 <liyang112@jd.com>
2025-02-14 13:54:41 +0800

    feat: add ASTHelper with a example

---
+++ src/Query/Parsers/ASTHelper.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <Parsers/IAST_fwd.h>
+#include <Core/Settings.h>
+#include <IO/WriteBufferFromString.h>
+#include <IO/ReadBufferFromString.h>
+
+namespace DB
+{
+
+using DB::IAST;
+using DB::ASTPtr;
+using DB::ASTs;
+using ConstASTPtr = std::shared_ptr<const IAST>;
+using ConstASTs = std::vector<ConstASTPtr>;
+
+namespace ErrorCodes
+{
+    extern const int LOGICAL_ERROR;
+    extern const int NOT_IMPLEMENTED;
+}
+
+#define APPLY_AST_TYPES(M) \
+    M(ASTDictionary)
+#define ENUM_TYPE(ITEM) ITEM,
+
+enum class ASTType : UInt8
+{
+    APPLY_AST_TYPES(ENUM_TYPE) UNDEFINED,
+};
+
+#undef ENUM_TYPE
+
+inline String toString(ASTType type)
+{
+    switch (type)
+    {
+#define ENUM_TYPE(ITEM) \
+    case ASTType::ITEM: \
+        return #ITEM;
+        APPLY_AST_TYPES(ENUM_TYPE)
+#undef ENUM_TYPE
+        default:
+            return "UNDEFINED";
+    }
+}
+
+ASTType getAstType(ASTPtr & ast)
+{
+    auto astSetQuery = std::dynamic_pointer_cast<ASTSetQuery>(ast);
+    if (astSetQuery)
+    {
+        return ASTType::ASTDictionary;  
+    }
+
+    //type not ASTSetQuery, need to continue
+
+    return ASTType::UNDEFINED;
+}
+
+}
+
