089552bb87ff01deebcb2df8d71a975fa375a314
wujianchao5 <wujianchao5@jd.com>
2025-04-15 07:23:15 +0000

    feat: add test exception_on_incorrect_pipeline

---
+++ src/Query/tests/gtest_exception_on_incorrect_pipeline.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+
+#include <Processors/Sources/SourceFromSingleChunk.h>
+#include <Processors/Sinks/NullSink.h>
+#include <Processors/Executors/PipelineExecutor.h>
+
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypesNumber.h>
+
+using namespace DB;
+
+TEST(Processors, PortsConnected_BC)
+{
+    auto col = ColumnUInt8::create(1, 1);
+    Columns columns;
+    columns.emplace_back(std::move(col));
+    Chunk chunk(std::move(columns), 1);
+
+    Block header = {ColumnWithTypeAndName(ColumnUInt8::create(), std::make_shared<DataTypeUInt8>(), "x")};
+
+    auto source = std::make_shared<SourceFromSingleChunk>(std::move(header), std::move(chunk));
+    auto sink = std::make_shared<NullSink>(source->getPort().getHeader());
+
+    connect(source->getPort(), sink->getPort());
+
+    auto processors = std::make_shared<Processors>();
+    processors->emplace_back(std::move(source));
+    processors->emplace_back(std::move(sink));
+
+    PipelineExecutor executor(processors, nullptr);
+    executor.execute(1, true);
+}
+
+TEST(Processors, PortsNotConnected_BC)
+{
+    auto col = ColumnUInt8::create(1, 1);
+    Columns columns;
+    columns.emplace_back(std::move(col));
+    Chunk chunk(std::move(columns), 1);
+
+    Block header = {ColumnWithTypeAndName(ColumnUInt8::create(), std::make_shared<DataTypeUInt8>(), "x")};
+
+    auto source = std::make_shared<SourceFromSingleChunk>(std::move(header), std::move(chunk));
+    auto sink = std::make_shared<NullSink>(source->getPort().getHeader());
+
+    /// connect(source->getPort(), sink->getPort());
+
+    auto processors = std::make_shared<Processors>();
+    processors->emplace_back(std::move(source));
+    processors->emplace_back(std::move(sink));
+
+#ifndef ABORT_ON_LOGICAL_ERROR
+    try
+    {
+        PipelineExecutor executor(processors, nullptr);
+        executor.execute(1, true);
+        ASSERT_TRUE(false) << "Should have thrown.";
+    }
+    catch (DB::Exception & e)
+    {
+        std::cout << e.displayText() << std::endl;
+        ASSERT_TRUE(e.displayText().find("pipeline") != std::string::npos) << "Expected 'pipeline', got: " << e.displayText();
+    }
+#endif
+}
