b3d66895fadcd836fa38abbbf9b9b5c3f5a3b9c4
zhangdongdong92 <zhangdongdong92@jd.com>
2025-04-16 12:00:20 +0000

    feat: serialize and deserialize for ASTIdentifier,ASTTableIdentifier,ASTWindowDefinition,ASTWindowListElement

--- src/Query/Parsers/ASTHelper.cpp
+++ src/Query/Parsers/ASTHelper.cpp
@@ -3,9 +3,12 @@
 #include <boost/algorithm/string/case_conv.hpp>
 #include <Query/ProtosHelper/PlanSerDerHelper.h>
 #include <Query/ProtosHelper/ASTSerDerHelper.h>
+#include "Interpreters/IdentifierSemantic.h"
 
 namespace DB
 {
+using std::make_shared;
+
 
 namespace ErrorCodes
 {
@@ -28,11 +31,11 @@ void astToLowerCase(const ASTPtr & ast)
     {
         boost::to_lower(casted_ast->name);
     }
-    else if (auto casted_ast = ast->as<ASTTableColumnReference>())
+    else if (auto * casted_ast = ast->as<ASTTableColumnReference>())
     {
         boost::to_lower(casted_ast->column_name);
     }
-    else if (auto casted_ast = ast->as<ASTQuantifiedComparisonExt>())
+    else if (auto * casted_ast = ast->as<ASTQuantifiedComparisonExt>())
     {
         boost::to_lower(casted_ast->alias);
         boost::to_lower(casted_ast->comparator);
@@ -51,15 +54,15 @@ void astToUpperCase(const ASTPtr & ast)
     {
         boost::to_upper(casted_ast->fields_str);
     }
-    else if ( auto casted_ast = ast->as<ASTProjectionDeclaration>())
+    else if ( auto * casted_ast = ast->as<ASTProjectionDeclaration>())
     {
         boost::to_upper(casted_ast->name);
     }
-    else if (auto casted_ast = ast->as<ASTTableColumnReference>())
+    else if (auto * casted_ast = ast->as<ASTTableColumnReference>())
     {
         boost::to_upper(casted_ast->column_name);
     }
-    else if (auto casted_ast = ast->as<ASTQuantifiedComparisonExt>())
+    else if (auto * casted_ast = ast->as<ASTQuantifiedComparisonExt>())
     {
         boost::to_upper(casted_ast->alias);
         boost::to_upper(casted_ast->comparator);
@@ -110,6 +113,78 @@ void serializeASTImpl(const IAST & ast, WriteBuffer & buf)
         serializeEnum(casted->kind, buf);
         serializeAST(casted->expression_list, buf);
     }
+    else if (auto * casted = ast->as<ASTIdentifier>())
+    {
+        //parent serialize
+        writeBinary(casted->alias, buf);
+        writeBinary(casted->prefer_alias_to_column_name, buf);
+ 
+        writeBinary(casted->full_name, buf);
+        writeBinary(casted->name_parts, buf);
+        if (casted->semantic)
+        {
+            writeBinary(true, buf);
+            writeBinary(casted->semantic->special, buf);
+            writeBinary(casted->semantic->can_be_alias, buf);
+            writeBinary(casted->semantic->covered, buf);
+            if (casted->semantic->membership)
+            {
+                writeBinary(true, buf);
+                writeBinary(casted->semantic->membership.value(), buf);
+            }
+            else
+                writeBinary(false, buf);
+        
+            writeBinary(casted->semantic->table, buf);
+            writeBinary(casted->semantic->legacy_compound, buf);
+        }
+    }
+    else if (auto * casted = ast->as<ASTTableIdentifier>())
+    {
+            //parent serialize
+            writeBinary(casted->alias, buf);
+            writeBinary(casted->prefer_alias_to_column_name, buf);
+     
+            writeBinary(casted->full_name, buf);
+            writeBinary(casted->name_parts, buf);
+            if (casted->semantic)
+            {
+                writeBinary(true, buf);
+                writeBinary(casted->semantic->special, buf);
+                writeBinary(casted->semantic->can_be_alias, buf);
+                writeBinary(casted->semantic->covered, buf);
+                if (casted->semantic->membership)
+                {
+                    writeBinary(true, buf);
+                    writeBinary(casted->semantic->membership.value(), buf);
+                }
+                else
+                    writeBinary(false, buf);
+            
+                writeBinary(casted->semantic->table, buf);
+                writeBinary(casted->semantic->legacy_compound, buf);
+            }
+    }
+    else if (auto * casted = ast->as<ASTWindowDefinition>())
+    {
+        writeBinary(casted->parent_window_name, buf);
+        serializeAST(casted->partition_by, buf);
+        serializeAST(casted->order_by, buf);
+        writeBinary(casted->frame_is_default, buf);
+        writeBinary(static_cast<UInt8>(casted->frame_type), buf);
+        writeBinary(static_cast<UInt8>(casted->frame_begin_type), buf);
+        serializeAST(casted->frame_begin_offset, buf);
+        writeBinary(casted->frame_begin_preceding, buf);
+        writeBinary(static_cast<UInt8>(casted->frame_end_type), buf);
+        serializeAST(casted->frame_end_offset, buf);
+        writeBinary(casted->frame_end_preceding, buf);
+    }
+    else if (auto * casted = ast->as<ASTWindowListElement>())
+    {
+        writeBinary(casted->name, buf);
+        serializeAST(casted->definition, buf);
+    }
+
     // todo wujianchao add more types
     else
         throw Exception(ErrorCodes::NOT_IMPLEMENTED, "Not implement serialize of {}", toString(getAstType(ast)));
@@ -119,11 +194,107 @@ ASTPtr deserializeASTImpl(ASTType type, ReadBuffer & buf)
 {
     switch (type)
     {
-        case ASTType::ASTArrayJoin:
-        {
+        case ASTType::ASTArrayJoin: {
             auto ast = std::make_shared<ASTArrayJoin>();
             deserializeEnum(ast->kind, buf);
             ast->expression_list = deserializeASTWithChildren(ast->children, buf);
+            return ast;
+        }
+        case ASTType::ASTIdentifier:
+        {
+            auto ast = make_shared<ASTIdentifier>("");
+            readBinary(ast->alias, buf);
+            readBinary(ast->prefer_alias_to_column_name, buf);
+
+            readBinary(ast->full_name, buf);
+            readBinary(ast->name_parts, buf);
+        
+            bool has_semantic;
+            readBinary(has_semantic, buf);
+            if (has_semantic)
+            {
+                ast->semantic = std::make_shared<IdentifierSemanticImpl>();
+                readBinary(ast->semantic->special, buf);
+                readBinary(ast->semantic->can_be_alias, buf);
+                readBinary(ast->semantic->covered, buf);
+
+                bool has_member;
+                readBinary(has_member, buf);
+                if (has_member){
+                    size_t member_tmp;
+                    readBinary(member_tmp, buf);
+                    ast->semantic->membership = member_tmp;
+                }
+
+                readBinary(ast->semantic->table, buf);
+                readBinary(ast->semantic->legacy_compound, buf);
+            }
+            return ast;
+        }
+        case ASTType::ASTTableIdentifier:
+        {
+            auto ast = std::make_shared<ASTTableIdentifier>("");
+            readBinary(ast->alias, buf);
+            readBinary(ast->prefer_alias_to_column_name, buf);
+
+            readBinary(ast->full_name, buf);
+            readBinary(ast->name_parts, buf);
+        
+            bool has_semantic;
+            readBinary(has_semantic, buf);
+            if (has_semantic)
+            {
+                ast->semantic = std::make_shared<IdentifierSemanticImpl>();
+                readBinary(ast->semantic->special, buf);
+                readBinary(ast->semantic->can_be_alias, buf);
+                readBinary(ast->semantic->covered, buf);
+
+                bool has_member;
+                readBinary(has_member, buf);
+                if (has_member){
+                    size_t member_tmp;
+                    readBinary(member_tmp, buf);
+                    ast->semantic->membership = member_tmp;
+                }
+
+                readBinary(ast->semantic->table, buf);
+                readBinary(ast->semantic->legacy_compound, buf);
+            }
+            return ast;
+        }
+        case ASTType::ASTWindowDefinition:
+        {
+            auto ast = std::make_shared<ASTWindowDefinition>();
+            readBinary(ast->parent_window_name, buf);
+            ast->partition_by = deserializeASTWithChildren(ast->children, buf);
+            ast->order_by = deserializeASTWithChildren(ast->children, buf);
+        
+            readBinary(ast->frame_is_default, buf);
+            UInt8 frame_type_num = 0;
+            readBinary(frame_type_num, buf);
+            ast->frame_type = static_cast<WindowFrame::FrameType>(frame_type_num);
+        
+            UInt8 frame_begin_type_num = 0;
+            readBinary(frame_begin_type_num, buf);
+            ast->frame_begin_type = static_cast<WindowFrame::BoundaryType>(frame_begin_type_num);
+        
+            ast->frame_begin_offset = deserializeAST(buf);
+            readBinary(ast->frame_begin_preceding, buf);
+        
+            UInt8 frame_end_type_num = 0;
+            readBinary(frame_end_type_num, buf);
+            ast->frame_end_type = static_cast<WindowFrame::BoundaryType>(frame_end_type_num);
+        
+            ast->frame_end_offset = deserializeAST(buf);
+            readBinary(ast->frame_end_preceding, buf);
+        
+            return ast;
+        }
+        case ASTType::ASTWindowListElement:
+        {
+            auto ast = std::make_shared<ASTWindowListElement>();
+            readBinary(ast->name, buf);
+            ast->definition = deserializeAST(buf);
             return ast;
         }
         // todo wujianchao add more types
@@ -144,4 +315,3 @@ ASTFunctionPtr makeASTFunctionWithVectorArgs(ASTFunctionPtr & ast, const String 
 }
 
 }
-
