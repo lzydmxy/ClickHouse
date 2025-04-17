ebe34ecf232cb3972000343417b57e7ee3f67bad
hongzhigao1 <hongzhigao1@jd.com>
2025-04-17 08:53:46 +0000

    feat: add proto for DataStream and ITransformingStep

--- src/Query/ProtosHelper/ProtosSerDerHelper.cpp
+++ src/Query/ProtosHelper/ProtosSerDerHelper.cpp
@@ -1,13 +1,56 @@
-#include <Query/ProtosHelper/ProtosSerDerHelper.h>
-
-#include <Query/ProtosHelper/DataTypeHelper.h>
+#include <Query/Processors/QueryPlan/QueryPlanStepHelper.h>
 #include <Query/ProtosHelper/FieldHelper.h>
-
-#include <Query/Protos/plan_node.pb.h>
-
+#include <Query/ProtosHelper/PlanSerDerHelper.h>
+#include <Query/ProtosHelper/ProtosSerDerHelper.h>
 
 namespace DB
 {
+
+void ProtosSerDerHelper::serializeToProtoBase(const ITransformingStep & step, Protos::ITransformingStep & proto)
+{
+    proto.set_step_description(step.step_description);
+    if (step.input_streams.empty())
+    {
+        DataStream stream{.header = Block()};
+        toProto(stream, *proto.mutable_input_stream());
+    }
+    else
+    {
+        toProto(step.input_streams.front(), *proto.mutable_input_stream());
+    }
+}
+
+// return step_description and base_input_stream
+std::pair<String, DataStream> ProtosSerDerHelper::deserializeFromProtoBase(const Protos::ITransformingStep & proto)
+{
+    auto step_description = proto.step_description();
+    DataStream input_stream;
+    fillFromProto(input_stream, proto.input_stream());
+    return std::make_pair(std::move(step_description), std::move(input_stream));
+}
+
+void ProtosSerDerHelper::toProto(const DataStream & data_stream, Protos::DataStream & proto)
+{
+    serializeHeaderToProto(data_stream.header, *proto.mutable_header());
+    std::sort(proto.mutable_distinct_columns()->begin(), proto.mutable_distinct_columns()->end());
+    proto.set_has_single_port(data_stream.has_single_port);
+    for (const auto & element : data_stream.sort_description)
+        toProto(element, *proto.add_sort_description());
+    proto.set_sort_scope(DataStreamSortScopeConverter::toProto(data_stream.sort_scope));
+}
+
+void ProtosSerDerHelper::fillFromProto(DataStream & data_stream, const Protos::DataStream & proto)
+{
+    data_stream.header = deserializeHeaderFromProto(proto.header());
+    data_stream.has_single_port = proto.has_single_port();
+    for (const auto & proto_element : proto.sort_description())
+    {
+        SortColumnDescription element;
+        fillFromProto(element, proto_element);
+        data_stream.sort_description.emplace_back(std::move(element));
+    }
+    data_stream.sort_scope = DataStreamSortScopeConverter::fromProto(proto.sort_scope());
+}
 
 void ProtosSerDerHelper::toProto(const NameAndTypePair & pair, Protos::NameAndTypePair & proto)
 {
@@ -26,7 +69,6 @@ void ProtosSerDerHelper::fillFromProto(NameAndTypePair & pair, const Protos::Nam
     if (proto.has_subcolumn_delimiter_position())
         pair.subcolumn_delimiter_position = proto.subcolumn_delimiter_position();
 }
-
 
 void ProtosSerDerHelper::toProto(const SortColumnDescription & sort_column_description, Protos::SortColumnDescription & proto)
 {
--- src/Query/ProtosHelper/ProtosSerDerHelper.h
+++ src/Query/ProtosHelper/ProtosSerDerHelper.h
@@ -1,13 +1,11 @@
-#include <Core/NamesAndTypes.h>
-#include <Core/SortDescription.h>
-
-
+#include <Processors/QueryPlan/ITransformingStep.h>
 
 namespace DB
 {
 
 namespace Protos
 {
+class DataStream;
 class NameAndTypePair;
 class SortColumnDescription;
 class FillColumnDescription;
@@ -19,6 +17,12 @@ public:
     ProtosSerDerHelper() = default;
     ~ProtosSerDerHelper() = default;
 
+    static void serializeToProtoBase(const ITransformingStep & step, Protos::ITransformingStep & proto);
+    static std::pair<String, DataStream> deserializeFromProtoBase(const Protos::ITransformingStep & proto);
+
+    static void toProto(const DataStream & data_stream, Protos::DataStream & proto);
+    static void fillFromProto(DataStream & data_stream, const Protos::DataStream & proto);
+
     static void toProto(const NameAndTypePair & pair, Protos::NameAndTypePair & proto);
     static void fillFromProto(NameAndTypePair & pair, const Protos::NameAndTypePair & proto);
 
@@ -27,7 +31,6 @@ public:
 
     static void toProto(const FillColumnDescription & pair, Protos::FillColumnDescription & proto);
     static void fillFromProto(FillColumnDescription & pair, const Protos::FillColumnDescription & proto);
-
 };
 
 }
