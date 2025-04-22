7f07ade72caa94b0e1e15ba61ed826e46a6df937
wujianchao5 <wujianchao5@jd.com>
2025-04-22 03:02:03 +0000

    feat: add serder methods for filter step

--- src/Query/Processors/QueryPlan/FilterStepExt.h
+++ src/Query/Processors/QueryPlan/FilterStepExt.h
@@ -24,6 +24,9 @@ public:
     static std::pair<ConstASTPtr, ConstASTPtr> splitLargeInValueList(const ConstASTPtr & filter, UInt64 limit);
     static std::vector<ConstASTPtr> removeLargeInValueList(const std::vector<ConstASTPtr> & filters, UInt64 limit);
 
+    void toProto(Protos::FilterStepExt & proto, bool for_hash_equals = false) const;
+    static std::shared_ptr<FilterStep> fromProto(const Protos::FilterStepExt & proto, ContextPtr context);
+
 private:
     ConstASTPtr filter;
 };
