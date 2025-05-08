9b2d658d8436d0c89daab47bd99fa4a7c2400ac9
hongzhigao1 <hongzhigao1@jd.com>
2025-05-08 07:51:09 +0000

    fix: add Proto for FinalSampleStepExt

--- src/Query/Processors/QueryPlan/FinalSampleStepExt.cpp
+++ src/Query/Processors/QueryPlan/FinalSampleStepExt.cpp
@@ -1,21 +1,18 @@
 #include <Query/Processors/QueryPlan/FinalSampleStepExt.h>
+#include <Query/ProtosHelper/ProtosSerDerHelper.h>
 
 namespace DB
 {
 void FinalSampleStepExt::toProto(Protos::FinalSampleStepExt & proto, bool) const
 {
-    //todo: hongzhigao, other feat: need impl ITransformingStep::serializeToProtoBase
-    //ITransformingStep::serializeToProtoBase(*proto.mutable_query_plan_base());
+    ProtosSerDerHelper::serializeToProtoBase(*this, *proto.mutable_query_plan_base());
     proto.set_sample_size(sample_size);
     proto.set_max_chunk_size(max_chunk_size);
 }
 
 std::shared_ptr<FinalSampleStepExt> FinalSampleStepExt::fromProto(const Protos::FinalSampleStepExt & proto, ContextPtr)
 {
-    //todo: hongzhigao, other feat: need impl ITransformingStep::deserializeFromProtoBase
-    //auto [step_description, base_input_stream] = ITransformingStep::deserializeFromProtoBase(proto.query_plan_base());
-    String step_description;
-    DataStream base_input_stream;
+    auto [step_description, base_input_stream] = ProtosSerDerHelper::deserializeFromProtoBase(proto.query_plan_base());
     auto sample_size = proto.sample_size();
     auto max_chunk_size = proto.max_chunk_size();
     auto step = std::make_shared<FinalSampleStepExt>(base_input_stream, sample_size, max_chunk_size);
