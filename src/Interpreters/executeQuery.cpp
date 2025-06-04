d8b4aec250cbd2913c768e29f43a2ad44f0d07d0
zhangwanyun1 <643044969@qq.com>
2025-06-04 16:08:19 +0800

    fix: error when insert data

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -758,7 +758,7 @@ static std::tuple<ASTPtr, BlockIO> executeQueryImpl(
         }
         else
         {
-            ParserQueryExt parser(end, settings.allow_settings_after_format_in_insert);
+            ParserQuery parser(end, settings.allow_settings_after_format_in_insert);
             /// TODO: parser should fail early when max_query_size limit is reached.
             ast = parseQuery(parser, begin, end, "", max_query_size, settings.max_parser_depth, settings.max_parser_backtracks);
             // DB::ASTReplaceVisitor::replace(ast);
