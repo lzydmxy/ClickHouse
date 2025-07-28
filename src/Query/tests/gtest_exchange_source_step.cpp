b66b3c3115f86b23b22c18981c3174f0d31453ee
zhangwanyun1 <zhangwanyun1@jd.com>
2025-07-28 17:36:25 +0800

    fix: failed test ExchangeSourceStepTest

--- src/Query/tests/gtest_exchange_source_step.cpp
+++ src/Query/tests/gtest_exchange_source_step.cpp
@@ -20,6 +20,7 @@
 #include <Query/Exchange/DataTrans/IBroadcastSender.h>
 #include <Query/Exchange/DataTrans/LocalBroadcastChannel.h>
 #include <Query/Exchange/ExchangeDataKey.h>
+#include <Query/Exchange/bRPC/BrpcApplication.h>
 #include <Query/Processors/QueryPlan/BuildQueryPipelineSettingsExt.h>
 #include <gtest/gtest.h>
 #include <Common/tests/gtest_global_context.h>
@@ -30,15 +31,53 @@ using namespace DB;
 namespace UnitTest
 {
 
+namespace
+{
+    void startBrpcServer(brpc::Server & server, BrpcExchangeReceiverRegistryService & service_impl)
+    {
+        if (server.AddService(&service_impl, brpc::SERVER_DOESNT_OWN_SERVICE) != 0)
+        {
+            LOG(ERROR) << "Fail to add service";
+            return;
+        }
+        LOG(INFO) << "Add service success";
+
+        // Start the server.
+        brpc::ServerOptions options;
+        options.idle_timeout_sec = -1;
+
+        if (server.Start(0, &options) != 0)
+        {
+            LOG(ERROR) << "Fail to start Server";
+            return;
+        }
+        LOG(INFO) << "Start Server on port " << server.listen_address().port;
+    }
+
+    void stopBrpcServer(brpc::Server & server)
+    {
+        server.Stop(1000);
+    }
+}
+
+
 TEST(ExchangeSourceStepTest, PipelineOneInput)
 {
+    // init bRPC server
+    Poco::AutoPtr<Poco::Util::MapConfiguration> map_config = new Poco::Util::MapConfiguration;
+    BrpcApplication::getInstance().initialize(*map_config);
+    brpc::Server server;
+    BrpcExchangeReceiverRegistryService service_impl(73400320);
+    startBrpcServer(server, service_impl);
+
     // auto log = getLogger("ExchangeSourceStepTest");
     // auto context = getInitContext();
     auto context = Context::createCopy(getInitContext());
     auto optimizer_context = context->getOptimizerContext();
     UInt64 query_tx_id = 666;
-    
-    AddressInfo local_address("localhost", 0, "test", "123456");
+    auto rpc_port = server.listen_address().port;
+    optimizer_context->setRPCPort(rpc_port);
+    AddressInfo local_address("localhost", 9999, "test", "123456", optimizer_context->getRPCPort());
     optimizer_context->setTransactionID(query_tx_id);
     optimizer_context->setPlanSegmentInstanceID({1, 1});
 
@@ -51,7 +90,7 @@ TEST(ExchangeSourceStepTest, PipelineOneInput)
     client_info.current_user = "test";
     client_info.initial_query_id = plan_segment.getQueryId();
 
-    auto coordinator_address = std::make_shared<AddressInfo>("localhost", 8888, "test", "123456");
+    auto coordinator_address = std::make_shared<AddressInfo>("localhost", 8888, "test", "123456", optimizer_context->getRPCPort());
     auto coordinator_address_str = extractExchangeHostPort(*(coordinator_address.get()));
     plan_segment.setCoordinatorAddress(*(coordinator_address.get()));
     optimizer_context->setCoordinatorAddress(coordinator_address);
@@ -78,7 +117,7 @@ TEST(ExchangeSourceStepTest, PipelineOneInput)
     ExchangeOptions exchange_options{.exchange_timeout_ts = tp, .send_threshold_in_bytes = 0};
     exchange_source_step.setExchangeOptions(exchange_options);
 
-    auto data_key_1 = std::make_shared<ExchangeDataKey>(query_tx_id, 1, 1);
+    auto data_key_1 = std::make_shared<ExchangeDataKey>(optimizer_context->getTransactionID(plan_segment.getQueryId()), 1, 1);
     BroadcastSenderProxyPtr local_sender_1 = BroadcastSenderProxyRegistry::instance().getOrCreate(data_key_1);
     local_sender_1->accept(context, header);
 
@@ -96,6 +135,8 @@ TEST(ExchangeSourceStepTest, PipelineOneInput)
         ASSERT_TRUE(status.code == BroadcastStatusCode::RUNNING);
     }
 
+    local_sender_1->finish(BroadcastStatusCode::ALL_SENDERS_DONE, "Finish");
+
     PullingAsyncPipelineExecutor executor(pipeline);
     Chunk pull_chunk;
     for (int i = 0; i < 3; i++)
@@ -105,16 +146,27 @@ TEST(ExchangeSourceStepTest, PipelineOneInput)
         ASSERT_TRUE(pull_chunk.bytes() == total_bytes);
     }
     executor.cancel();
+
+    stopBrpcServer(server);
 }
 
 TEST(ExchangeSourceStepTest, PipelineMultiInput)
 {
+    // init bRPC server
+    Poco::AutoPtr<Poco::Util::MapConfiguration> map_config = new Poco::Util::MapConfiguration;
+    BrpcApplication::getInstance().initialize(*map_config);
+    brpc::Server server;
+    BrpcExchangeReceiverRegistryService service_impl(73400320);
+    startBrpcServer(server, service_impl);
+
     auto log = getLogger("ExchangeSourceStepTest");
     auto context = getInitContext();
     auto optimizer_context = context->getOptimizerContext();
     UInt64 query_tx_id = 666;
     // auto context = Context::createCopy(global_context);
-    AddressInfo local_address("localhost", 0, "test", "123456");
+    auto rpc_port = server.listen_address().port;
+    optimizer_context->setRPCPort(rpc_port);
+    AddressInfo local_address("localhost", 0, "test", "123456", optimizer_context->getRPCPort());
     optimizer_context->setTransactionID(query_tx_id);
     optimizer_context->setPlanSegmentInstanceID({1, 1});
 
@@ -127,7 +179,7 @@ TEST(ExchangeSourceStepTest, PipelineMultiInput)
     client_info.current_user = "test";
     client_info.initial_query_id = plan_segment.getQueryId();
 
-    auto coordinator_address = std::make_shared<AddressInfo>("localhost", 8888, "test", "123456");
+    auto coordinator_address = std::make_shared<AddressInfo>("localhost", 8888, "test", "123456", optimizer_context->getRPCPort());
     auto coordinator_address_str = extractExchangeHostPort(*(coordinator_address.get()));
     plan_segment.setCoordinatorAddress(*(coordinator_address.get()));
     optimizer_context->setCoordinatorAddress(coordinator_address);
@@ -153,12 +205,12 @@ TEST(ExchangeSourceStepTest, PipelineMultiInput)
     auto tp = getDeltaTimePoint(1000);
     ExchangeOptions exchange_options{.exchange_timeout_ts = tp, .send_threshold_in_bytes = 0};
     exchange_source_step.setExchangeOptions(exchange_options);
-
-    auto data_key_1 = std::make_shared<ExchangeDataKey>(query_tx_id, 1, 1);
+    auto query_unique_id = optimizer_context->getTransactionID(plan_segment.getQueryId());
+    auto data_key_1 = std::make_shared<ExchangeDataKey>(query_unique_id, 1, 1);
     BroadcastSenderProxyPtr local_sender_1 = BroadcastSenderProxyRegistry::instance().getOrCreate(data_key_1);
     local_sender_1->accept(context, header);
 
-    auto data_key_2 = std::make_shared<ExchangeDataKey>(query_tx_id, 2, 1);
+    auto data_key_2 = std::make_shared<ExchangeDataKey>(query_unique_id, 2, 1);
     BroadcastSenderProxyPtr local_sender_2 = BroadcastSenderProxyRegistry::instance().getOrCreate(data_key_2);
     local_sender_2->accept(context, header);
 
@@ -196,6 +248,8 @@ TEST(ExchangeSourceStepTest, PipelineMultiInput)
         ASSERT_TRUE(pull_chunk.bytes() == total_bytes);
     }
     executor.cancel();
+    stopBrpcServer(server);
 }
 
 }
+
