9f06c7195eebd666694cd27ebfc6837c72c1ae07
hongzhigao1 <hongzhigao1@jd.com>
2025-09-26 14:58:04 +0800

    feat: make soft link for contrib when compile with docker

--- pipline.sh
+++ pipline.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-mkdir -p /ClickHouse_new/build
 cd /ClickHouse_new
-rm -rf contrib
-cp -r /root/contrib .
+# 运行流水线编译的镜像时，将宿主机的`/root/ClickHouse_new`目录挂载到容器中：`-v /root/ClickHouse_new:/root/ClickHouse_new`
+rm -rf contrib && ln -s /root/ClickHouse_new/contrib/ contrib
+mkdir build
 cmake -S . -B build
 cmake --build build
 objcopy --strip-debug build/programs/clickhouse
